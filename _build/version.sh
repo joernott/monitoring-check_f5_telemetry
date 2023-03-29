edef5c6b5b4d5bfd9168f0f16239aef831c085c5
Ott, Jörn WI (Fa. Allgeier Experts / Goetzfried) <Joern.Ott@schufa.de>
2023-03-29 12:37:10 +0200

    bump version

--- _build/version.sh
+++ _build/version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # see https://github.com/joernott/monitoring-check_f5_telemetry/tags
 APP="monitoring-check_f5_telemetry"
-VERSION="0.0.2"
+VERSION="0.0.3"
 RELEASE="1"
 URL="https://github.com/joernott/monitoring-check_f5_telemetry/archive/refs/tags/v${VERSION}.tar.gz"
 ARCH="x86_64"
